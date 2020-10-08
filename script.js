22387e2d33c9f3582317349f5ed166f7d3aeb5f9
Gianluigi Vitale <gianluigi.vitale11@gmail.com>
2020-10-08 18:39:34 +0200

    cards generated inside a div, title in h2, body p, only unique cards

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-const items = document.querySelectorAll('a');
-items.forEach(item => {
-	item.addEventListener('click',(e) => {
-		// console.log(item.dataset.sku);
-		var dataSku = item.dataset.sku;
+const posts = document.querySelectorAll('#card-trigger li a');
+posts.forEach(post => {
+	post.addEventListener('click',(e) => {
+		// console.log(post.dataset.sku);
+		var dataSku = post.dataset.sku;
 
         ajaxCall(dataSku);
 
 	})
 })
+
+
 
 
 
@@ -21,12 +23,36 @@ function ajaxCall(dataSku) {
         if (this.readyState == 4 && this.status == 200) {
             var post = JSON.parse(xhttp.responseText);
 
-            var tag = document.createElement("p");
-            tag.setAttribute('data-sku', dataSku);
-            var text = document.createTextNode(`${post.title} ${post.body}`);
-            tag.appendChild(text);
-            var element = document.getElementById("cards-holder");
-            element.appendChild(tag);
+
+            var cardPresente = false;
+
+            const cards = document.querySelectorAll('#cards-holder div');
+            cards.forEach(card => {
+            	var cardData = card.dataset.sku;
+                if (cardData == dataSku) {
+                    cardPresente = true;
+                }
+            })
+
+            if (!cardPresente) {
+
+                var divCard = document.createElement("div");
+                divCard.setAttribute('data-sku', dataSku);
+                divCard.setAttribute('class', 'card');
+
+                var cardH2 = document.createElement("h2");
+                var cardTitle = document.createTextNode(post.title);
+                cardH2.appendChild(cardTitle);
+                divCard.appendChild(cardH2);
+
+                var cardP = document.createElement("p");
+                var cardBody = document.createTextNode(post.body);
+                cardP.appendChild(cardBody);
+                divCard.appendChild(cardP);
+
+                var cardHolder = document.getElementById("cards-holder");
+                cardHolder.appendChild(divCard);
+            }
         }
     };
 }
